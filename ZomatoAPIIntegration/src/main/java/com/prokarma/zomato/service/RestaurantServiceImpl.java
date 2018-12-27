f18b4d20f4fdfdf1e4a60a1d209a05fa2b9a8c54
nswarnalatha <nswarnalatha@nswarnalatha-lp.pkrmsezdc.com>
2018-12-27 20:38:55 +0530

    committing new changes

--- ZomatoAPIIntegration/src/main/java/com/prokarma/zomato/service/RestaurantServiceImpl.java
+++ ZomatoAPIIntegration/src/main/java/com/prokarma/zomato/service/RestaurantServiceImpl.java
@@ -2,7 +2,6 @@ package com.prokarma.zomato.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
 import com.prokarma.zomato.dao.RestaurantDAO;
 import com.prokarma.zomato.dto.Restaurant;
 
@@ -19,7 +18,6 @@ public class RestaurantServiceImpl implements RestaurantService {
 
 	@Override
 	public Restaurant getRestaurantDataById(Integer restaurantId) {
-		// TODO Auto-generated method stub
 		Restaurant restaurant = null;
 		restaurant = restaurantDAO.getRestaurantDataById(restaurantId);
 		return restaurant;
